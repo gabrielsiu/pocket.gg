d91240944d907121203cdc3160c96adbe7ec808f
Gabriel Siu <gabriel.siu@hotmail.com>
2020-01-31 09:22:26 -0500

    Starting fresh

--- pocket.gg/Pods/Target Support Files/Pods-pocket.gg/Pods-pocket.gg-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_pocket_gg : NSObject
-@end
-@implementation PodsDummy_Pods_pocket_gg
-@end
---
+++ pocket.gg/pocket.gg/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  pocket.gg
+//
+//  Created by Gabriel Siu on 2020-01-31.
+//  Copyright © 2020 Gabriel Siu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
